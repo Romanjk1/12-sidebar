9c616b61d2d72016016514db697fd700ab021196
Roman <olomukororoman@gmail.com>
2024-01-02 10:19:27 +0100

    first commit

---
+++ starter/src/App.jsx
@@ -0,0 +1,15 @@
+import Modal from './Modal'
+import SideBar from './SideBar'
+
+import Home from './assets/Home'
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <SideBar />
+    </>
+  )
+}
+export default App
